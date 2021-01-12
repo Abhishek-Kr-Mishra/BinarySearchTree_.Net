7296cb87a451d54bc5db267ea81ec430884a23d3
abh224272 <abh224272@gmail.com>
2021-01-12 12:22:52 +0530

    [Abhishek Mishra] Add: Method To add values in BST

---
+++ BinarySearchTreeProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinarySearchTreeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Binary Search Tree");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertData(56);
+            binarySearchTree.InsertData(30);
+            binarySearchTree.InsertData(70);
+
+            binarySearchTree.Display(binarySearchTree.GetRoot());
+        }
+    }
+}
